2eff4103e881f7093f2799f044bd1dafa7713dcd
nicole gumula <ngumula@guilford.edu>
2023-04-25 10:49:25 -0400

    password

--- src/main/java/edu/guilford/passwordGenerator.java
+++ src/main/java/edu/guilford/passwordGenerator.java
@@ -1,60 +1,90 @@
 package edu.guilford;
 
+import java.util.Scanner;
+
 public class passwordGenerator {
-    
-    // attributes
-    private String petName;
-    private String favColor;
-    private String birthday;
-    private String password;
+    public static void main(String[] args) { 
+    Scanner input = new Scanner(System.in);
 
-    // constructor
-    public passwordGenerator(String petName, String favColor, String birthday) {
-        this.petName = petName;
-        this.favColor = favColor;
-        this.birthday = birthday;
-        this.password = petName.substring(0,2) + favColor.substring(0,2) + birthday.substring(0,2);
-    }
+    // declare two new users
+    User user1 = new User(); 
+    User user2 = new User();
 
-    // getters and setters
-    public String getPetName() {
-        return petName;
-    }
+    // Ask for user1's information 
+    System.out.println("Please enter your first name: ");
+    user1.setFirstName(input.nextLine());
 
-    public void setPetName(String petName) {
-        this.petName = petName;
-    }
+    System.out.println("Please enter your last name: ");
+    user1.setLastName(input.nextLine());
 
-    public String getFavColor() {
-        return favColor;
-    }
+    System.out.println("Please enter your email address: ");
+    user1.setEmail(input.nextLine());
 
-    public void setFavColor(String favColor) {
-        this.favColor = favColor;
-    }
+    System.out.println("What is the name of your favorite pet");
+    user1.setPetName(input.nextLine());
 
-    public String getBirthday() {
-        return birthday;
-    }
+    System.out.println("What is your favorite color?");
+    user1.setFavColor(input.nextLine());
 
-    public void setBirthday(String birthday) {
-        this.birthday = birthday;
-    }
+    System.out.println("What is your birthday?");
+    user1.setBirthday(input.nextLine());
 
-    public String getPassword() {
-        return password;
-    }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    // Ask for user2's information
+    System.out.println("Please enter your first name: ");
+    user2.setFirstName(input.nextLine());
 
-    // toString
-    @Override
-    public String toString() {
-        return "passwordGenerator [birthday=" + birthday + ", favColor=" + favColor + ", password=" + password
-                + ", petName=" + petName + "]";
-    }
+    System.out.println("Please enter your last name: ");
+    user2.setLastName(input.nextLine());
 
-    
+    System.out.println("Please enter your email address: ");
+    user2.setEmail(input.nextLine());
+
+    System.out.println("What is the name of your favorite pet");
+    user2.setPetName(input.nextLine());
+
+    System.out.println("What is your favorite color?");
+    user2.setFavColor(input.nextLine());
+
+    System.out.println("What is your birthday?");
+    user2.setBirthday(input.nextLine());
+
+    // Generate passwords for each user
+    user1.setPassword(user1.createPassword());
+    user2.setPassword(user2.createPassword());
+
+    // Print out the passwords
+    System.out.println("User 1's password is: " + user1.getPassword());
+    System.out.println("User 2's password is: " + user2.getPassword());
+
+    // ask each user for a secret key
+    System.out.println("User 1, please enter your secret key: ");
+    String key1 = input.nextLine();
+
+    System.out.println("User 2, please enter your secret key: ");
+    String key2 = input.nextLine();
+
+    // print out the secret keys
+    System.out.println("User 1's secret key is: " + key1);
+    System.out.println("User 2's secret key is: " + key2);
+
+    // create encrypted passwords using the secret key
+    String encryptedPassword1 = AES.encrypt(user1.getPassword(), key1);
+    String encryptedPassword2 = AES.encrypt(user2.getPassword(), key2);
+
+    // print out the encrypted passwords
+    System.out.println("User 1's encrypted password is: " + encryptedPassword1);
+    System.out.println("User 2's encrypted password is: " + encryptedPassword2);
+
+    // create decrypted passwords using the secret key
+    String decryptedPassword1 = AES.decrypt(encryptedPassword1, key1);
+    String decryptedPassword2 = AES.decrypt(encryptedPassword2, key2);
+
+    // print out the decrypted passwords
+    System.out.println("User 1's decrypted password is: " + decryptedPassword1);
+    System.out.println("User 2's decrypted password is: " + decryptedPassword2);
+
+}
+
+
 }
